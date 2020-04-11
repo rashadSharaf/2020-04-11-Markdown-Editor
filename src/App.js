338e3f7f181496fbf8290878cfb6ebe31d8eeaca
RashadS <shar0489@algonquinlive.com>
2020-04-11 14:29:49 -0400

    created converter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { HashRouter } from "react-router-dom";
+let marked = require("marked")
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    //initial state values
+    this.state = {
+      input: "",
+    };
+  }
+
+  updateInput = (ev) => {
+    let input = ev.target.value;
+    this.setState({ input });
+    console.log(this.state.input);
+  };
+
+  render() {
+
+    return (
+      <div>
+        <HashRouter basename="/">
+          <div style={{ display: "flex", flexDirection: "row" }}>
+            <form
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                width: "50%",
+                height: "100vh",
+                margin: "1rem",
+              }}
+            >
+              <label>Please Type in your code here</label>
+              <textarea
+                rows="50"
+                style={{ padding: "1rem" }}
+                value={this.state.input}
+                onChange={this.updateInput}
+              />
+            </form>
+            <div
+              style={{
+                display: "flex",
+                margin: "1rem",
+                flexDirection: "column",
+                width: "50%",
+              }}
+            >
+              <h4> HTML OUTPUT </h4>
+              <div dangerouslySetInnerHTML = {{__html: marked(this.state.input)}}  style={{padding:"1rem", height: "100vh", border: "3px solid black" }}>
+                
+              </div>
+            </div>
+          </div>
+        </HashRouter>
+      </div>
+    );
+  }
 }
-
-export default App;
